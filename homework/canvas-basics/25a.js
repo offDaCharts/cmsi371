01dbf666f1936f762bb3a9ccc831ba9aec1d9e3b
Quin <rqthames@yahoo.com>
2013-01-22 16:11:48 -0800

    Problem 25a: Blue square in center of canvas

---
+++ homework/canvas-basics/25a.js
@@ -0,0 +1,9 @@
+(function () {
+    var canvas = document.getElementById("canvas"),
+        renderingContext = canvas.getContext("2d");
+
+    //Blue square
+    renderingContext.fillStyle = "blue";
+    renderingContext.fillRect(206, 206, 100, 100);
+
+}());
