79c865479e059ae6b89aafa90db557c99a46a84e
Quin <rqthames@yahoo.com>
2013-01-22 19:34:49 -0800

    --Problem 26a: grid of lavender squares

---
+++ homework/canvas-basics/26a.js
@@ -0,0 +1,15 @@
+(function () {
+    var canvas = document.getElementById("canvas"),
+        renderingContext = canvas.getContext("2d"),
+        squareSize = 10;
+
+    //Lavendar
+    renderingContext.fillStyle = "#E6E6FA";
+    
+    for(var x = 0; x < canvas.width; x+=squareSize + 1) {
+        for(var y = 0; y < canvas.height; y+=squareSize + 1) {
+            renderingContext.fillRect(x, y, squareSize, squareSize);
+        }
+    }
+
+}());
