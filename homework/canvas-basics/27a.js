fa130c4461002f0c83c29e88526c56192947f965
Quin <rqthames@yahoo.com>
2013-01-26 22:32:46 -0800

    -Created 27a: green wire frame cube, and 27e: planet with ring

---
+++ homework/canvas-basics/27a.js
@@ -0,0 +1,33 @@
+(function () {
+    var canvas = document.getElementById("canvas"),
+        renderingContext = canvas.getContext("2d"),
+        current = {x: 256, y: 256},
+        sideLength = 100,
+        angle = Math.PI/6,
+        offset = {x: 0.75 * Math.cos(angle), y: 0.75 * Math.sin(angle)};
+        
+    renderingContext.beginPath();
+    renderingContext.moveTo(current.x, current.y);
+    renderingContext.lineTo(current.x + offset.x * sideLength, current.y - offset.y * sideLength);
+    renderingContext.lineTo(current.x + offset.x * sideLength, current.y - (offset.y + 1) * sideLength);
+    renderingContext.moveTo(current.x, current.y);
+    renderingContext.lineTo(current.x, current.y -= sideLength);
+    renderingContext.lineTo(current.x + offset.x * sideLength, current.y - offset.y * sideLength);
+    renderingContext.lineTo(current.x + (offset.x - 1) * sideLength, current.y - offset.y * sideLength);
+    renderingContext.moveTo(current.x, current.y);
+    renderingContext.lineTo(current.x -= sideLength, current.y);
+    renderingContext.lineTo(current.x + offset.x * sideLength, current.y - offset.y * sideLength);
+    renderingContext.lineTo(current.x + offset.x * sideLength, current.y - (offset.y - 1) * sideLength);
+    renderingContext.moveTo(current.x, current.y);
+    renderingContext.lineTo(current.x, current.y += sideLength);
+    renderingContext.lineTo(current.x + offset.x * sideLength, current.y - offset.y * sideLength);
+    renderingContext.lineTo(current.x + (offset.x + 1) * sideLength, current.y - offset.y * sideLength);
+    renderingContext.moveTo(current.x, current.y);
+    renderingContext.lineTo(current.x += sideLength, current.y);
+
+    
+    renderingContext.lineWidth = 1;
+    renderingContext.strokeStyle = "green";
+    renderingContext.stroke();
+
+}());
---
+++ homework/canvas-basics/27e.js
@@ -0,0 +1,44 @@
+(function () {
+    var canvas = document.getElementById("canvas"),
+        renderingContext = canvas.getContext("2d"),
+        radialGradientPlanet = renderingContext.createRadialGradient(210, 160, 1, 230, 180, 320),
+        radialGradientRing = renderingContext.createRadialGradient(210, 160, 1, 230, 180, 320),
+        circleCenter = {x: 306, y: 256},
+        circleRadius = 200,
+        ringStartOffset = {x: 256, y: 0},
+        controlPointOffset = {x: 256, y: 50};
+    
+    radialGradientPlanet.addColorStop(0, "white");
+    radialGradientPlanet.addColorStop(1, "blue");
+    radialGradientRing.addColorStop(0, "white");
+    radialGradientRing.addColorStop(1, "red");
+
+    
+    //Draw back half of ring
+    renderingContext.beginPath();
+    renderingContext.moveTo(circleCenter.x + ringStartOffset.x, circleCenter.y - ringStartOffset.y);
+    renderingContext.quadraticCurveTo(circleCenter.x + controlPointOffset.x, circleCenter.y - controlPointOffset.y, circleCenter.y, circleCenter.y - controlPointOffset.y);
+    renderingContext.quadraticCurveTo(circleCenter.x - controlPointOffset.x, circleCenter.y - controlPointOffset.y, circleCenter.x - ringStartOffset.x, circleCenter.y - ringStartOffset.y);
+
+    
+    renderingContext.lineWidth = 25;
+    renderingContext.strokeStyle = radialGradientRing;
+    renderingContext.stroke();
+
+    //Draw planet
+    renderingContext.fillStyle = radialGradientPlanet;
+    renderingContext.beginPath();
+    renderingContext.arc(circleCenter.x, circleCenter.y, circleRadius, 0, Math.PI * 2, true);
+    renderingContext.fill();
+    
+    //Draw front half of ring
+    renderingContext.beginPath();
+    renderingContext.moveTo(circleCenter.x + ringStartOffset.x, circleCenter.y - ringStartOffset.y);
+    renderingContext.quadraticCurveTo(circleCenter.x + controlPointOffset.x, circleCenter.y + controlPointOffset.y, circleCenter.y, circleCenter.y + controlPointOffset.y);
+    renderingContext.quadraticCurveTo(circleCenter.x - controlPointOffset.x, circleCenter.y + controlPointOffset.y, circleCenter.x - ringStartOffset.x, circleCenter.y - ringStartOffset.y);
+    
+    
+    renderingContext.strokeStyle = radialGradientRing;
+    renderingContext.stroke();
+
+}());
